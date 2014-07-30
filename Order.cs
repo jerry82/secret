80748263502d89a483b3988fb266e09df9f68a35
jerry <hatrungn@yahoo.com>
2014-07-29 23:32:54 -0400

    first commit
    first commit

---
+++ Order.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AmazonInterview
+{
+    #region entity
+    public class Order
+    {
+        public long OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public float TotalCharge { get; set; } 
+        public List<Product> OrderedProducts { get; set; }
+        public List<ValidatorResult> ValidatorResult { get; set; }
+        public bool Reject { get; set; } 
+    }
+
+    public class Customer
+    {
+        public long CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; } 
+    }
+
+    public class Product
+    {
+        public long ProductId { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; } 
+    }
+
+    /// <summary>
+    /// this class stores the validation result when a validator is applied to 
+    /// an order
+    /// </summary>
+    public class ValidatorResult
+    {
+        public long ValidatorId { get; set; }
+        public ValidatorCodeEnum Code { get; set; }
+        public string Message { get; set; } 
+    }
+
+    public enum ValidatorCodeEnum
+    {
+        OK = 1, 
+        InvalidQuantity = 2, 
+        InvalidCharge = 3, 
+        OutOfStock = 4,
+        //...
+    }
+    #endregion
+
+    #region logic
+    /// <summary>
+    /// individual validator need to implement this abstract class 
+    /// </summary>
+    public abstract class Validator
+    {
+        public int Id { get; set; } 
+
+        abstract public ValidatorResult Validate(Order order);
+    }
+
+    /// <summary>
+    /// this class provide an api to 
+    /// 1. register validators with the system
+    /// 2. apply validators to an order
+    /// 3. reject the order if any validator reject the order
+    /// </summary>
+    public class OrderValidator
+    {
+        private Dictionary<int, Validator> _validators = new Dictionary<int,Validator>(); 
+
+        /// <summary>
+        /// function helps register the validator
+        /// </summary>
+        /// <param name="validator"></param>
+        public void RegisterValidator(Validator validator) 
+        {
+            if (!_validators.ContainsKey(validator.Id))
+            {
+                _validators.Add(validator.Id, validator);
+            }
+        }
+
+        /// <summary>
+        /// function helps un-register a validator
+        /// </summary>
+        /// <param name="id"></param>
+        public void RemoveValidator(int id)
+        {
+            if (_validators.ContainsKey(id))
+                _validators.Remove(id);
+        }
+
+        /// <summary>
+        /// function helps apply validators to the order
+        /// </summary>
+        /// <param name="order"></param>
+        public void ApplyValidators(Order order)
+        {
+            if (order.ValidatorResult == null)
+                order.ValidatorResult = new List<ValidatorResult>();
+
+            //if there's no registered validator
+            if (_validators.Count == 0) 
+            {
+                ValidatorResult result = new ValidatorResult() { Code = ValidatorCodeEnum.OK };
+                order.ValidatorResult.Add(result);
+
+                return;
+            }
+
+            foreach (Validator validator in _validators.Values)
+            {
+                ValidatorResult result = validator.Validate(order);
+                order.ValidatorResult.Add(result);    
+            }
+        }
+
+        /// <summary>
+        /// function handles reject order
+        /// </summary>
+        /// <param name="order"></param>
+        public void RejectOrder(Order order)
+        {
+            order.Reject = false;
+
+            foreach (ValidatorResult result in order.ValidatorResult)
+            {
+                if (result.Code != ValidatorCodeEnum.OK)
+                {
+                    order.Reject = true;
+                    break;
+                }
+            }
+
+            if (order.Reject)
+            {
+                //logic to handle order rejection goes here
+                //...
+            }
+        }
+    }
+    #endregion   
+}
---
+++ Question2.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AmazonInterview
+{
+    public class Record
+    {
+        public string StudentId { get; set; }
+        public string TestId { get; set; }
+        public double TestScore { get; set; } 
+    }
+
+    public class FinalScore
+    {
+        public string StudentId { get; set; }
+        public double Score { get; set; } 
+    }
+
+    public class ScoreQueue 
+    {
+        public int Top { get; set; }
+        
+        //this is a sorted score
+        public List<double> Scores { get; set; }
+   
+        /// <summary>
+        /// calculate the average score, sometimes Top will > Scores.Count
+        /// </summary>
+        /// <returns></returns>
+        public double GetAverage()
+        {
+            if (Scores.Count == 0)
+                return 0.0;
+
+            //sort scores in ascending order
+            MergeSort.Ascend(Scores);
+
+            //calculate the average
+            int lim = Top < Scores.Count ? Top : Scores.Count;
+            double sum = 0.0;
+            for (int i = 0; i < lim; i++)
+            {
+                sum += Scores[Scores.Count - 1 - i];
+            }
+
+            return sum / lim;
+        }
+    }
+
+    /// <summary>
+    /// main class to calculate the student final score
+    /// </summary>
+    public class Calculator 
+    {
+        public void BuildScoreDict(Dictionary<string, ScoreQueue> studentScoreDict, List<Record> records, int top)
+        {
+            foreach (Record record in records)
+            {
+                string studentId = record.StudentId;
+
+                if (!studentScoreDict.ContainsKey(studentId))
+                {
+                    studentScoreDict.Add(studentId, new ScoreQueue() { Top = top });
+                }
+
+                ScoreQueue queue = studentScoreDict[studentId];
+                queue.Scores.Add(record.TestScore);
+            }
+        }
+
+        /// <summary>
+        /// build score dictionary take O(X): X is the number of record
+        /// calculate the score of student takes: O(N * K * log(K)):
+        /// N : is the number of student
+        /// K is the number of tests 
+        /// time to merge sort the score of K test, it takes K * log(K)
+        /// 
+        /// total time: O(X) + O(N * K * log(K)) ~ O(N * K * log(K))
+        /// </summary>
+        /// <param name="records"></param>
+        /// <returns></returns>
+        public List<FinalScore> CalculateFinalScoreFromRecords(List<Record> records)
+        {
+            Dictionary<string, ScoreQueue> studentScoreDict = new Dictionary<string, ScoreQueue>();
+            List<FinalScore> finalScores = new List<FinalScore>();
+
+            //build the dictionary 
+            BuildScoreDict(studentScoreDict, records, 5);
+
+            //calculate individual score
+            foreach (string studentId in studentScoreDict.Keys)
+            {
+                ScoreQueue queue = (ScoreQueue)studentScoreDict[studentId];
+                FinalScore finalScore = new FinalScore();
+                finalScore.StudentId = studentId;
+                finalScore.Score = queue.GetAverage();
+
+                finalScores.Add(finalScore);
+            }
+
+            return finalScores;
+        }
+    }
+
+    #region helpers
+    public class MergeSort
+    {
+        public static void Ascend(List<double> arr)
+        {
+            List<double> result = DoMerge(arr, 0, arr.Count - 1);
+            for (int i = 0; i < result.Count; i++)
+            {
+                arr[i] = result[i];
+            }
+        }
+
+        private static List<double> DoMerge(List<double> l, int start, int end)
+        {
+            List<double> result = new List<double>();
+            if (start == end)
+            {
+                result.Add(l[start]);
+                return result;
+            }
+
+            int mid = (int)((start + end) / 2);
+            List<double> left = DoMerge(l, start, mid);
+            List<double> right = DoMerge(l, mid + 1, end);
+
+            result = Merge(left, right);
+            return result;
+        }
+
+        private static List<double> Merge(List<double> a, List<double> b)
+        {
+            List<double> result = new List<double>();
+
+            int iA = 0;
+            int iB = 0;
+
+            while (result.Count < a.Count + b.Count)
+            {
+                if (iA >= a.Count && iB < b.Count)
+                {
+                    result.Add(b[iB++]);
+                }
+                else if (iB >= b.Count && iA < a.Count)
+                {
+                    result.Add(a[iA++]);
+                }
+                else
+                {
+                    if (a[iA] < b[iB])
+                        result.Add(a[iA++]);
+                    else
+                        result.Add(b[iB++]);
+                }
+            }
+
+            return result;
+        }
+    }
+    #endregion
+}
